fcdb5df4c320e38698efbb5be2dff097a1495cd0
Rémy Machavoine <remygt@hotmail.fr>
2023-03-15 13:02:25 +0100

    add start of first usecase create reservation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,14 +1,13 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.example.use_case.CreateReservation;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        CreateReservation createReservation = new CreateReservation();
+        createReservation.execute();
     }
 
     public String getMessage() {
---
+++ src/main/java/com/example/use_case/CreateReservation.java
@@ -0,0 +1,8 @@
+package com.example.use_case;
+
+public class CreateReservation {
+
+    public void execute() {
+        System.out.println("CreateReservation");
+    }
+}
