68fcf29c486f5ba85b9f94ede647dde8a5cfe418
Rémy Machavoine <remygt@hotmail.fr>
2023-03-17 09:47:18 +0100

    delete code in main

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,25 +3,7 @@ package com.example;
 public class App
 {
     public static void main( String[] args ) {
-        /*Reservations reservations = new ReservationsInMemory();
-        Rooms rooms = new RoomsInMemory();
-        ProspectDao prospectDao = new ProspectDaoInMemory();
-
-        CreateReservationCommand createReservationCommand = new CreateReservationCommand(reservations, rooms, prospectDao);
-        CreateReservation createReservation = new CreateReservation(
-            LocalDateTime.of(2021, 1, 1, 10, 0),
-                LocalDateTime.of(2021, 1, 1, 11, 0),
-            "1",
-            "1",
-            2,
-            new String[] {"pomme@gmail.com"},
-            "ça à l'air d'être un super espace de travail"
-        );
-        try {
-            createReservationCommand.execute(createReservation);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }*/
+        System.out.println( "Hello World!" );
     }
 
     public String getMessage() {
--- src/test/java/infrastructure/IdGeneratorNotRandom.java
+++ src/test/java/infrastructure/IdGeneratorNotRandom.java
@@ -5,6 +5,6 @@ import com.example.use_case.common.IdGenerator;
 public class IdGeneratorNotRandom implements IdGenerator {
     @Override
     public String generate() {
-        return "1";
+        return "23833299328";
     }
 }
