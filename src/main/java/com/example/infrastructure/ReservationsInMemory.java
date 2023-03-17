b6585e9b201b92252ad25cea4130c923a6f928a8
Quentin MOLERO <quentin.molero@outlook.com>
2023-03-17 09:54:10 +0100

    Fix add in reservation list

--- src/main/java/com/example/infrastructure/ReservationsInMemory.java
+++ src/main/java/com/example/infrastructure/ReservationsInMemory.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ReservationsInMemory implements Reservations {
-    final List<Reservation> reservations = Arrays.asList(
+    final List<Reservation> reservations = new ArrayList<>(Arrays.asList(
             new Reservation(new ReservationId("1"), new TimeWindow(LocalDateTime.of(2024, 1, 1, 10, 0), LocalDateTime.of(2024, 1, 1, 11, 0)), new RoomId("1"), new ProspectId("1"), 2, ""),
             new Reservation(new ReservationId("2"), new TimeWindow(LocalDateTime.of(2024, 1, 1, 11, 0), LocalDateTime.of(2024, 1, 1, 12, 0)), new RoomId("1"), new ProspectId("1"), 3, ""),
             new Reservation(new ReservationId("3"), new TimeWindow(LocalDateTime.of(2024, 1, 1, 12, 0), LocalDateTime.of(2024, 1, 1, 13, 0)), new RoomId("1"), new ProspectId("1"), 1, ""),
@@ -18,11 +18,10 @@ public class ReservationsInMemory implements Reservations {
             new Reservation(new ReservationId("7"), new TimeWindow(LocalDateTime.of(2024, 1, 1, 16, 0), LocalDateTime.of(2024, 1, 1, 17, 0)), new RoomId("2"), new ProspectId("1"), 1, "desc"),
             new Reservation(new ReservationId("8"), new TimeWindow(LocalDateTime.of(2024, 1, 1, 17, 0), LocalDateTime.of(2024, 1, 1, 18, 0)), new RoomId("2"), new ProspectId("1"), 1, "desc"),
             new Reservation(new ReservationId("9"), new TimeWindow(LocalDateTime.of(2024, 1, 1, 18, 0), LocalDateTime.of(2024, 1, 1, 19, 0)), new RoomId("2"), new ProspectId("1"), 1, "desc")
-    );
+    ));
 
     @Override
     public Reservation create(Reservation reservation) {
-
         this.reservations.add(reservation);
         return reservation;
     }
